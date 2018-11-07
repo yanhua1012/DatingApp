013620cce5e4158f978f55471b010010992a0bc2
yanhua1012 <yanhua1012@gmail.com>
2018-11-07 10:53:14 +0800

    initial Commit

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    // [Route("api/[controller]")]
+    // [ApiController]
+    // public class AuthController:
+    // {
+    //     public AuthController(IAuthRepository repo)
+    //     {
+            
+    //     }
+    // }
+}
