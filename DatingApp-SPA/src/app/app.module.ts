736c53493a4859eee01d7b6c92a8124589d1f33d
Terry Wang 王彥驊 <1512010@iisigroup.com>
2018-11-15 17:03:23 +0800

    Child to Parent Component

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      RegisterComponent,
+      HomeComponent
    ],
    imports: [
       BrowserModule,
