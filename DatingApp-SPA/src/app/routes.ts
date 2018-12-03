74669060f3f4868b11f28014547fb1ab3913dfac
Terry Wang 王彥驊 <1512010@iisigroup.com>
2018-12-03 15:54:27 +0800

    multipile routes wiath a single route

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -6,11 +6,17 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    { path: 'home', component : HomeComponent },
-    { path: 'members', component : MemberListComponent, canActivate: [AuthGuard] },
-    { path: 'messages', component : MessagesComponent },
-    { path: 'lists', component : ListsComponent },
-    { path: 'home', component : HomeComponent },
-    { path: 'home', component : HomeComponent },
-    { path: '**', redirectTo : 'home', pathMatch: 'full' }
+    { path: '', component : HomeComponent },
+    {
+        // path: 'dummy', // localhost:4200/dummymembers
+        path: '', // localhost:4200/dummymembers
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component : MemberListComponent },
+            { path: 'messages', component : MessagesComponent },
+            { path: 'lists', component : ListsComponent },
+        ]
+    },
+    { path: '**', redirectTo : '', pathMatch: 'full' }
 ];
