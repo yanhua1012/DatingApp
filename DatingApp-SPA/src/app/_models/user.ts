f2d8edfcb6625b714dc8356d9efb5e5054ea9f8f
Terry Wang 王彥驊 <1512010@iisigroup.com>
2018-12-07 14:58:22 +0800

    retrieving member list

--- DatingApp-SPA/src/app/_models/user.ts
+++ DatingApp-SPA/src/app/_models/user.ts
@@ -3,7 +3,7 @@ import { Photo } from './photo';
 export interface User {
     id: number;
     username: string;
-    kownAs: string;
+    knownAs: string;
     age: number;
     gender: string;
     created: Date;
